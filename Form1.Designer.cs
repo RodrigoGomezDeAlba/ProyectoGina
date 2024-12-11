6cc39fb4bed84f0fd96e07f7d7cce9ec03c34269
JuanManuelFriasCortes <juanmanuelfriascortes@gmail.com>
2024-12-10 18:08:51 -0600

    Se agregó el pr

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,105 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            BTNPortadaSalir = new Button();
+            CCB = new PictureBox();
+            UAALOGO = new PictureBox();
+            DATOS = new PictureBox();
+            FECHALBL = new Label();
+            LOGOSLOGAN = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)CCB).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)UAALOGO).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)DATOS).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)LOGOSLOGAN).BeginInit();
+            SuspendLayout();
+            // 
+            // BTNPortadaSalir
+            // 
+            BTNPortadaSalir.Location = new Point(19, 381);
+            BTNPortadaSalir.Name = "BTNPortadaSalir";
+            BTNPortadaSalir.Size = new Size(102, 49);
+            BTNPortadaSalir.TabIndex = 0;
+            BTNPortadaSalir.Text = "Salir";
+            BTNPortadaSalir.UseVisualStyleBackColor = true;
+            BTNPortadaSalir.Click += BTNPortadaSalir_Click;
+            // 
+            // CCB
+            // 
+            CCB.BackColor = Color.Snow;
+            CCB.Location = new Point(19, 12);
+            CCB.Name = "CCB";
+            CCB.Size = new Size(747, 341);
+            CCB.TabIndex = 1;
+            CCB.TabStop = false;
+            // 
+            // UAALOGO
+            // 
+            UAALOGO.BackColor = Color.Snow;
+            UAALOGO.Location = new Point(658, 12);
+            UAALOGO.Name = "UAALOGO";
+            UAALOGO.Size = new Size(108, 88);
+            UAALOGO.TabIndex = 2;
+            UAALOGO.TabStop = false;
+            // 
+            // DATOS
+            // 
+            DATOS.BackColor = Color.Snow;
+            DATOS.Location = new Point(412, 82);
+            DATOS.Name = "DATOS";
+            DATOS.Size = new Size(354, 213);
+            DATOS.TabIndex = 3;
+            DATOS.TabStop = false;
+            // 
+            // FECHALBL
+            // 
+            FECHALBL.AutoSize = true;
+            FECHALBL.BackColor = Color.Snow;
+            FECHALBL.Location = new Point(645, 313);
+            FECHALBL.Name = "FECHALBL";
+            FECHALBL.Size = new Size(50, 20);
+            FECHALBL.TabIndex = 4;
+            FECHALBL.Text = "label1";
+            // 
+            // LOGOSLOGAN
+            // 
+            LOGOSLOGAN.BackColor = Color.Snow;
+            LOGOSLOGAN.Location = new Point(328, 12);
+            LOGOSLOGAN.Name = "LOGOSLOGAN";
+            LOGOSLOGAN.Size = new Size(182, 147);
+            LOGOSLOGAN.TabIndex = 5;
+            LOGOSLOGAN.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Sienna;
+            ClientSize = new Size(800, 450);
+            Controls.Add(LOGOSLOGAN);
+            Controls.Add(FECHALBL);
+            Controls.Add(DATOS);
+            Controls.Add(UAALOGO);
+            Controls.Add(CCB);
+            Controls.Add(BTNPortadaSalir);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)CCB).EndInit();
+            ((System.ComponentModel.ISupportInitialize)UAALOGO).EndInit();
+            ((System.ComponentModel.ISupportInitialize)DATOS).EndInit();
+            ((System.ComponentModel.ISupportInitialize)LOGOSLOGAN).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button BTNPortadaSalir;
+        private PictureBox CCB;
+        private PictureBox UAALOGO;
+        private PictureBox DATOS;
+        private Label FECHALBL;
+        private PictureBox LOGOSLOGAN;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -5,6 +5,25 @@ namespace ProyectoGina
         public Form1()
         {
             InitializeComponent();
+
+            CCB.SizeMode= PictureBoxSizeMode.Zoom; // Ajusta la imagen al PictureBox
+            CCB.Image = Image.FromFile("C:\\Users\\manue\\OneDrive\\Documentos\\UAA\\Semestre 3\\Programacion 2\\Proyecto final\\ProyectoGina/logo-ccb.png");
+            UAALOGO.SizeMode = PictureBoxSizeMode.Zoom; // Ajusta la imagen al PictureBox
+            UAALOGO.Image = Image.FromFile("C:\\Users\\manue\\OneDrive\\Documentos\\UAA\\Semestre 3\\Programacion 2\\Proyecto final\\ProyectoGina/UAALOGO.png");
+            DATOS.SizeMode = PictureBoxSizeMode.Zoom; // Ajusta la imagen al PictureBox
+            DATOS.Image = Image.FromFile("C:\\Users\\manue\\OneDrive\\Documentos\\UAA\\Semestre 3\\Programacion 2\\Proyecto final\\ProyectoGina/Datos.png");
+            LOGOSLOGAN.SizeMode = PictureBoxSizeMode.Zoom; // Ajusta la imagen al PictureBox
+            LOGOSLOGAN.Image = Image.FromFile("C:\\Users\\manue\\OneDrive\\Documentos\\UAA\\Semestre 3\\Programacion 2\\Proyecto final\\ProyectoGina/LOGOSLOGAN.png");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FECHALBL.Text = DateTime.Now.ToString("dd/MM/yyyy"); // Muestra la fecha en formato día/mes/año
+        }
+
+        private void BTNPortadaSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
